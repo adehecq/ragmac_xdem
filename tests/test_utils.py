d1a6c78123d5b702a1d3d77beca2753dcc5e3588
adehecq <3285905+adehecq@users.noreply.github.com>
2021-12-13 16:38:07 +0100

    Add a test for ASTER parsing date

---
+++ tests/test_utils.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from ragmac_xdem import utils
+
+
+def test_aster_date():
+    """
+    Test parsing ASTER date from filename works as expected
+    """
+    assert utils.get_aster_date("DEM_2000.00000000.tif") == datetime(2000, 1, 1, 0, 0)
+    assert utils.get_aster_date("DEM_2001.50000000.tif") == datetime(2001, 7, 2, 12, 0)
+    assert utils.get_aster_date("DEM_2019.99999999.tif") == datetime(2019, 12, 31, 23, 59, 59, 684643)
