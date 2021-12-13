6773a9d1b48673555bf6566ef83fa89689cf72e9
adehecq <3285905+adehecq@users.noreply.github.com>
2021-12-13 16:41:24 +0100

    Minor fixes to previous commit

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+"""
+Test suite for utils.py
+"""
 from datetime import datetime
 
 from ragmac_xdem import utils
@@ -5,7 +8,7 @@ from ragmac_xdem import utils
 
 def test_aster_date():
     """
-    Test parsing ASTER date from filename works as expected
+    Test that parsing ASTER date from filename works as intended.
     """
     assert utils.get_aster_date("DEM_2000.00000000.tif") == datetime(2000, 1, 1, 0, 0)
     assert utils.get_aster_date("DEM_2001.50000000.tif") == datetime(2001, 7, 2, 12, 0)
