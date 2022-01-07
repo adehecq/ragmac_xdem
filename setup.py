519af9b7d3984aadbc1bbc231a9549d09d3f7061
Friedrich Knuth <friedrichknuth@users.noreply.github.com>
2022-01-07 13:13:39 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(
         "write_to": "ragmac_xdem/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    }
+    },
+    install_requires=[
+        'geoutils @ git+https://github.com/GlacioHack/GeoUtils.git@main#egg=GeoUtils',
+        'xdem @ git+https://github.com/GlacioHack/xdem.git@main#egg=xdem',
+    ]
 )
