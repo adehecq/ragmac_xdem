fd12fcf55ed5fa2dfc1d80a1c1dcb69292148fee
Friedrich <knuth@uw.edu>
2022-02-16 23:35:19 -0800

    add utility to update and gather plots

---
+++ scripts/update_plots.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+## Utility to run experiments and update plots.
+## Simplest solution for now since experiments check for processed products so no major reprocessing is done.
+
+## Main experiments
+# python main_experiment1.py -c CH_Aletschgletscher
+# python main_experiment1.py -c AT_Hintereisferner
+# python main_experiment2.py -c PK_Baltoro
+
+## Optional experiments
+# python main_experiment1.py -c NO_Vestisen
+# python main_experiment2.py -c RU_FJL
+# python main_experiment2.py -c CL_NPI
+
+# Move files to flat directory and zip
+outdir="plots/mb_figs/"
+mkdir -pv $outdir
+find ../data/processed/ -name "*mb_fig.png" -type f | while read -r file
+do
+   new_file=${file/"../data/processed/"/}
+   new_file=${new_file//\//_} #replace slashes with dashes
+   cp "$file" "$outdir$new_file"
+done
+
+outdir="plots/ddem_figs/"
+mkdir $outdir
+find ../data/processed/ -name "*ddem_fig.png" -type f | while read -r file
+do
+   new_file=${file/"../data/processed/"/}
+   new_file=${new_file//\//_} #replace slashes with dashes
+   cp "$file" "$outdir$new_file"
+done
+
+tar cvzf plots.tar.gz plots/
