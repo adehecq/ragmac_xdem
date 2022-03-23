e6f52a43ef66214d21c6bab0ff9d4d3065006d48
Friedrich <knuth@uw.edu>
2022-03-23 15:35:41 -0700

    send dashboard to custom port

--- ragmac_xdem/io.py
+++ ragmac_xdem/io.py
@@ -15,14 +15,15 @@ import logging
 @author: friedrichknuth
 """
 
-def dask_start_cluster(nproc, threads=1, ip_addres=None):
+def dask_start_cluster(nproc, threads=1, ip_addres=None, port=':8786'):
     """
     Starts a dask cluster. Can provide a custom IP or URL to view the progress dashboard. 
     This may be necessary if working on a remote machine.
     """
     cluster = LocalCluster(n_workers=nproc,
-                       threads_per_worker=threads,
-                       silence_logs=logging.ERROR)
+                           threads_per_worker=threads,
+                           silence_logs=logging.ERROR,
+                           dashboard_address=port)
 
     client = Client(cluster)
     
